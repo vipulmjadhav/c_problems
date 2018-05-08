99a054a4adfb383d58e59bebac1e12a4641560d0
vipulmjadhav <jadhavipul933@gmail.com>
2018-05-08 13:43:47 +0530

    added comment

--- pointer_struct.c
+++ pointer_struct.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 
-
 struct demo
 {
 	int arr[4];
@@ -23,10 +22,11 @@ int main()
 	printf("*(p+2) - %d\n",*(p+2));
 	printf("*(p+3) - %d\n",*(p+3));
 	
-	printf("%d\n",*(p+4));
+	printf("a - %d\n",*(p+4));
 	
 	printf("*(p+5) -> %d\n",*(p+5));
-	printf("%d\n",q->a);
+	printf("q->a \t q.a     \n%d\t%d\n",q->a,(*q).a);
+	printf("q->arr[2] - %d\n",q->arr[2]);
 	printf("%d\n",*(p+3)+q->a-*r);
 
 	printf("%d\t%d\t%d\t%d\n",*(p+3),*(obj.arr+3),*(r+1),*((q->arr)+3));
@@ -34,3 +34,17 @@ int main()
 
 return 0;
 }
+
+/* OUTPUT 
+
+structure pointer is used as -> operator.
+
+The 'arrow' operator is syntactic sugar. bar->member is the same as (*bar).member
+
+arrow operator is used as dereferencing operaor in case of pointers .
+pointers to an object is used to access the members of the structure then arrow is used.
+pointer_to_obj -> member.
+
+dot operator is used to access the mebers of object when object itself is used to access the members of it.
+obj.member
+*/
